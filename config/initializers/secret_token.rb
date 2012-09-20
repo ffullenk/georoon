4f1a07620affe1651e42ea0f1af55e3fb6711d0e
francisco <francisco000@gmail.com>
2012-09-20 16:47:48 -0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Georoom::Application.config.secret_token = '94993952e9813fa08650b34594afea2d1eec3a4256bea053acb9dd4f202048fd2897d0d88df27a2138324db078bc65317c25b73a5200d3d823b20eda6f521b8e'
